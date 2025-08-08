719d16ef79ec4c35e3aab1ff660a33822ed97aad
Tshiamo Legote <wametshiamo@gmail.com>
2025-08-08 13:41:58 +0200

    company website

---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ display: 'flex', gap: '1rem', padding: '10px', background: '#eee' }}>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/services">Services</Link>
+      <Link to="/contact">Contact</Link>
+    </nav>
+  );
+}
+export default Navbar;
